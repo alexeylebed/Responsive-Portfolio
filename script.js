a7b34aebc6dd9fbb543d6f0cbf80acbd265f7607
Alexey Lebed <lebeddos@gmail.com>
2018-09-26 17:55:05 -0700

    2

---
+++ script.js
@@ -0,0 +1,16 @@
+let menu = document.getElementById("navinner");
+let burger = document.getElementById("burger");
+let hidemenu = document.getElementById("hidemenu");
+
+
+
+burger.onclick = function(){
+    menu.style.display = 'block';
+};
+
+hidemenu.onclick = function () {
+    menu.style.display = 'none';
+ };
+
+
+console.log(body);
